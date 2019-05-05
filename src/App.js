0ea004507d3751c09979e7ad37147f109d12e032
Isaías De la Hoz <isaiasdelahoz@MacBook-Pro-de-Isaias.local>
2019-05-05 09:49:49 -0500

    iterate over array

--- src/App.js
+++ src/App.js
@@ -3,22 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+	const names = ["Pedro", "Juan", "Isaías"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	<div>
+      <h1>Hola mundo</h1>
+      <p>paragrahp</p>
+      <ul>
+      {names.map(name => 
+      	<li>{name}</li>
+      	)}
+      </ul>
     </div>
   );
 }
