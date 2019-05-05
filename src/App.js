cd5624be8c0d2a80244a79883b579b4d81f66d57
Isaías De la Hoz <isaiasdelahoz@MacBook-Pro-de-Isaias.local>
2019-05-05 11:34:20 -0500

    component with constructor

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import Welcome from './Welcome.js';
+import React, {Component} from 'react';
 import './App.css';
 
 
 
-function App() {
-	const names = ["Pedro", "Juan", "German"];
-  return (
-  	<div>
-  		{names.map(name =>
-  			<Welcome name={name} />
-  		)}
-  	</div>
-  );
+class App extends Component {
+
+	constructor(props) {
+	  super(props);
+	  // Don't call this.setState() here!
+	  this.state = { title: "Hola Mundo desde Estado" };
+	}
+
+	render() {
+			return (
+			<div>
+		  	<h1> {this.state.title} </h1>
+		  	<button onClick={this.changeTitle.bind(this)}>Cambie el título</button>
+	  	</div>
+  	);
+	}
+
+	changeTitle() {
+		this.setState({
+			title: "Nuevo título"
+		});
+	}
 }
 
 export default App;
